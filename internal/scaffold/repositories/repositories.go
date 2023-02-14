b8239125eabe896efadb7f588d452db5bb57cd08
Pedro Lucas Lima <lclpedro@gmail.com>
2023-02-13 23:17:46 -0300

    fix: adjusting and rename package

--- internal/scaffold/repositories/repositories.go
+++ internal/scaffold/repositories/repositories.go
@@ -6,7 +6,7 @@ import (
 	"github.com/lclpedro/scaffold-golang-fiber/internal/scaffold/repositories/health"
 	"github.com/lclpedro/scaffold-golang-fiber/internal/scaffold/repositories/person"
 	"github.com/lclpedro/scaffold-golang-fiber/pkg/mysql"
-	uow "github.com/lclpedro/scaffold-golang-fiber/pkg/unity_of_work"
+	uow "github.com/lclpedro/scaffold-golang-fiber/pkg/unit_of_work"
 )
 
 type AllRepositories struct {
@@ -15,7 +15,7 @@ type AllRepositories struct {
 	Address          address.Repository
 }
 
-func RegistryRepositories(uow uow.UnityOfWorkInterface, dbConnection mysql.Connection) uow.UnityOfWorkInterface {
+func RegistryRepositories(uow uow.UnitOfWorkInterface, dbConnection mysql.Connection) uow.UnitOfWorkInterface {
 	uow.Register("HealthRepository", func(tx *sqlx.Tx) interface{} {
 		repo := health.NewHealthRepository(dbConnection)
 		return repo
