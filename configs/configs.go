1644c908f0369921b5828ecd6de2261a004a8bc7
Pedro Lucas Lima <lclpedro@gmail.com>
2023-05-04 07:28:39 -0300

    feat: adjusting factory connection and Unit of work factory

--- configs/configs.go
+++ configs/configs.go
@@ -3,7 +3,7 @@ package configs
 import "github.com/spf13/viper"
 
 func InitConfigs() {
-	viper.AddConfigPath("./configs")
+	viper.AddConfigPath("../configs")
 	viper.SetConfigName("config") // Register config file name (no extension)
 	viper.SetConfigType("json")   // Look for specific type
 	err := viper.ReadInConfig()
