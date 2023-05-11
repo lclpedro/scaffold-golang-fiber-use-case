8cd21392a2bed8b6cb226ed8045e301d1d362ba3
Pedro Lucas Lima <lclpedro@gmail.com>
2023-05-10 21:23:49 -0300

    feat: adjusting typing transaction

--- internal/scaffold/repositories/repositories.go
+++ internal/scaffold/repositories/repositories.go
@@ -1,8 +1,7 @@
 package repositories
 
 import (
-	"database/sql"
-
+	"github.com/jmoiron/sqlx"
 	"github.com/lclpedro/scaffold-golang-fiber/internal/scaffold/repositories/address"
 	"github.com/lclpedro/scaffold-golang-fiber/internal/scaffold/repositories/health"
 	"github.com/lclpedro/scaffold-golang-fiber/internal/scaffold/repositories/person"
@@ -16,17 +15,17 @@ type AllRepositories struct {
 }
 
 func RegistryRepositories(uow mysql.UnitOfWorkInterface, dbConnection mysql.Connection) mysql.UnitOfWorkInterface {
-	uow.Register("HealthRepository", func(tx *sql.Tx) interface{} {
+	uow.Register("HealthRepository", func(tx *sqlx.Tx) interface{} {
 		repo := health.NewHealthRepository(dbConnection)
 		return repo
 	})
 
-	uow.Register("PersonRepository", func(tx *sql.Tx) interface{} {
+	uow.Register("PersonRepository", func(tx *sqlx.Tx) interface{} {
 		repo := person.NewPersonRepository(dbConnection)
 		return repo
 	})
 
-	uow.Register("AddressRepository", func(tx *sql.Tx) interface{} {
+	uow.Register("AddressRepository", func(tx *sqlx.Tx) interface{} {
 		repo := address.NewAddressRepository(dbConnection)
 		return repo
 	})
--- pkg/mysql/connection.go
+++ pkg/mysql/connection.go
@@ -11,7 +11,7 @@ import (
 type connection struct {
 	read  *sqlx.DB
 	write *sqlx.DB
-	tx    *sql.Tx
+	tx    *sqlx.Tx
 }
 
 var (
@@ -27,7 +27,7 @@ type Connection interface {
 	QueryRow(query string, args ...interface{}) *sql.Row
 	Close() error
 	GetDB() *sqlx.DB
-	SetTx(tx *sql.Tx)
+	SetTx(tx *sqlx.Tx)
 	RemoveTx()
 }
 
@@ -48,7 +48,7 @@ func (conn *connection) GetDB() *sqlx.DB {
 	return conn.write
 }
 
-func (conn *connection) SetTx(tx *sql.Tx) {
+func (conn *connection) SetTx(tx *sqlx.Tx) {
 	conn.tx = tx
 }
 
@@ -66,15 +66,18 @@ func (conn *connection) Exec(query string,
 	args ...interface{}) (result sql.Result, err error) {
 	if conn.tx != nil {
 		result, err = conn.tx.Exec(query, args...)
-	} else {
-		result, err = conn.write.Exec(query, args...)
+		return
 	}
+	result, err = conn.write.Exec(query, args...)
 	return
 }
 
 func (conn *connection) NamedExec(query string,
 	arg interface{}) (result sql.Result, err error) {
-
+	if conn.tx != nil {
+		result, err = conn.tx.NamedExec(query, arg)
+		return
+	}
 	result, err = conn.write.NamedExec(query, arg)
 	return
 }
